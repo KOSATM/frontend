1425e9364cf5e55bad8d1b7251831d0e38eb41ee
dlgudwn55 <dkrlekeheoek@naver.com>
2025-11-11 15:28:59 +0900

    fix: 호텔 추천 페이지, 여행 계획 요약 페이지 링크 연결

--- src/router/planner.js
+++ src/router/planner.js
@@ -1,6 +1,7 @@
 import PlannerCreate from "@/views/planner/PlannerCreate.vue";
 import TravelPlanForm from "@/views/planner/TravelPlanForm.vue";
-
+import HotelRecommendation from "@/views/planner/HotelRecommendation.vue";
+import TripSummary from "@/views/planner/TripSummary.vue";
 
 const planner = [
   {
@@ -12,6 +13,16 @@ const planner = [
     path: '/planner/travelplan',
     name: 'travelplan',
     component: TravelPlanForm,
+  },
+  {
+    path: '/planner/hotel',
+    name: 'hotel',
+    component: HotelRecommendation,
+  },
+  {
+    path: '/planner/summary',
+    name: 'summary',
+    component: TripSummary,
   }
 
 ];
