4d1b5711d48ea5fba86a9299e8d2107c2cc20c9d
so0126 <nada981226@gmail.com>
2025-11-25 21:54:00 +0900

    fix: axios는 한 곳에서만 임포트하도록 수정

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap'
 import '@/assets/styles/global.scss'
 
+
 createApp(App)
   .use(createPinia())
   .use(router)
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require('path')
-
 module.exports = defineConfig({
   transpileDependencies: true,
-
   devServer: {
-    port: 80,
-  },
-
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('~bootstrap', path.resolve(__dirname, 'node_modules/bootstrap'))
-      .set('@', path.resolve(__dirname, 'src'))
-  },
-})
+      port: 80
+    }
+});
