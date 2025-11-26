20f7402bca8540e428150df8ddf870e8b2b8e37f
so0126 <nada981226@gmail.com>
2025-11-26 15:08:19 +0900

    feat: create Review Post 생성

--- src/api/travelgramApi.js
+++ src/api/travelgramApi.js
@@ -1,5 +1,11 @@
 import api from './axios'
 
+// 0) 리뷰 포스트 생성 (review_posts auto increment 생성)
+export const createReview = async(travelPlanId)=>{
+  const res = await api.post('/review/create', {travelPlanId})
+  return res.data
+} 
+
 /* 1) 리뷰 사진 그룹 생성 (review_photo_group auto increment 생성) */
 export const createReviewPhotoGroup = async () => {
   const res = await api.post('/review/photos/group')
@@ -10,3 +16,4 @@ export const createReviewPhotoGroup = async () => {
 export const uploadReviewPhotos = async (formData) => {
   return api.post('/review/photos/upload', formData, {
 })}
+
