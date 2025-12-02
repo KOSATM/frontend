ad4e4c031b814ffb97315ec9e54b9c989230086a
so0126 <nada981226@gmail.com>
2025-12-02 15:33:52 +0900

    fix: export 필요 없는 부분 삭제

--- src/api/travelgramApi.js
+++ src/api/travelgramApi.js
@@ -1,18 +1,18 @@
 import api from './axios'
 
 // 1) 리뷰 포스트 + 리뷰 사진 그룹 생성 (review_posts, review_photo_groups auto increment 생성)
-export const createReview = async(planId)=>{
+const createReview = async(planId)=>{
   const res = await api.post('/reviews/create', {planId})
   return res.data
 } 
 
 /* 2) 리뷰 사진 업로드 */
-export const uploadReviewPhotos = async (formData) => {
+const uploadReviewPhotos = async (formData) => {
   return api.post('/reviews/photos/upload', formData, {
 })}
 
 // 3) 리뷰 사진 순서 업데이트
-export const updatePhotoOrder = async (payload) => {
+const updatePhotoOrder = async (payload) => {
   return api.put('/reviews/photo/order', payload)
 }
 
