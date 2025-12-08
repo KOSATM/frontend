8c05aedb731afa8f65b748a217d13edb79e45ee9
so0126 <nada981226@gmail.com>
2025-12-08 19:11:04 +0900

    feat: getBlogList api 추가

--- src/api/plannerApi.js
+++ src/api/plannerApi.js
@@ -4,6 +4,16 @@ async function getActivePlan(userId) {
   return api.get(`/plans/${userId}/active/detail`)
 }
 
+const getBlogList=async(keyword, userId)=>{
+    const res = await api.get('api/search/blog',{
+        params:{
+            keyword: keyword,
+            userId: userId
+        }
+    })
+    return res.data
+}
 export default {
-  getActivePlan
+  getActivePlan,
+  getBlogList
 };
