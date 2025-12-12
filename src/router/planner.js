05c5aaba6a2ed8e2f74c33ca3b6be31c744cb40f
so0126 <nada981226@gmail.com>
2025-12-12 14:57:21 +0900

    feat: planner 관련 UI 추가

--- src/router/planner.js
+++ src/router/planner.js
@@ -3,7 +3,8 @@ import CreatePlan from "@/views/planner/CreatePlan.vue";
 import PlanList from "@/views/planner/edit/PlanList.vue";
 import Payment from "@/views/planner/hotel/Payment.vue";
 import BookingComplete from "@/views/planner/BookingComplete.vue";
-
+import PlannerSummary from "@/components/planner/PlannerSummary.vue"
+import PlannerTest from "@/components/planner/PlannerTest.vue"
 const planner = [
 
   {
@@ -12,12 +13,12 @@ const planner = [
     component: CreatePlan,
     meta: { title: "새 플랜 생성" },
   },
-  // {
-  //   path: "/planner/travelplan",
-  //   name: "travelplan",
-  //   component: TravelPlanForm,
-  //   meta: { title: "여행 계획 작성" },
-  // },
+  {
+    path: "/planner/summary",
+    name: "plansummary",
+    component: PlannerSummary,
+    meta: { title: "플랜 요약" },
+  },
   {
     path: "/planner/edit",
     name: "planedit",
@@ -45,6 +46,12 @@ const planner = [
     name: "bookingComplete",
     component: BookingComplete,
     meta: { title: "예약 완료" },
+  },
+  {
+    path: "/planner/test",
+    name: "plannertest",
+    component: PlannerTest,
+    meta: { title: "플래너 테스트" },
   },
 ];
 
