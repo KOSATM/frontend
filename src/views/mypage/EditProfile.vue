2843f4442d7e8dd34e529bc750aa27af8021b0c4
so0126 <nada981226@gmail.com>
2025-12-12 10:44:13 +0900

    refactor: mypage edit 에서 백헤더 제거

--- src/views/mypage/EditProfile.vue
+++ src/views/mypage/EditProfile.vue
@@ -2,7 +2,7 @@
   <div class="mypage-page">
 
     <PageHeader title="MyPage" subtitle="나의 여행 정보" icon="bi-person" />
-    <BackButtonPageHeader title="Edit Profile" subtitle="나의 정보를 수정해보세요." @back="goBack"/>
+    <!-- <BackButtonPageHeader title="Edit Profile" subtitle="나의 정보를 수정해보세요." @back="goBack"/> -->
 
     <div class="text-center mb-5">
       <div class="position-relative d-inline-block mb-3">
@@ -132,7 +132,7 @@ import { ref, reactive, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import BaseSection from '@/components/common/BaseSection.vue'
 import PageHeader from "@/components/common/PageHeader.vue";
-import BackButtonPageHeader from '@/components/common/BackButtonPageHeader.vue'
+// import BackButtonPageHeader from '@/components/common/BackButtonPageHeader.vue'
 import NavigationButtons from '@/components/common/button/NavigationButtons.vue';
 
 
